b729ada3c89e2afc557cd72b915d7f6b2a8590b8
Kite <ixkaito@gmail.com>
2023-10-18 12:00:26 +0900

    Add HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      jost: "'Jost', sans-serif",
+      fraunces: "'Fraunces', serif",
+      italiana: "'Italiana', sans-serif",
+    },
     extend: {},
   },
   plugins: [],
